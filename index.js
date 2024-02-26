462387b851d5e06ade6b1fad46d5d26b111a8dfb
Will Metzler <metzlerwill@gmail.com>
2024-02-26 12:27:54 -0500

    added code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Will is the champion';
+
+document.body.appendChild(newHeader);
+
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
